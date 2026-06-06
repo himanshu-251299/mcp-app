e161f925c0cb7c41a70eeaebc9da82c46c24bce6
himanshu_45 <himanshu.yadav@cyberevolve.com>
2026-06-07 00:54:49 +0530

    Add toolcallbackprovider for mcp

--- src/main/java/com/mcp_app/controller/AiController.java
+++ src/main/java/com/mcp_app/controller/AiController.java
@@ -2,7 +2,9 @@ package com.mcp_app.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/ai")
 public class AiController {
 
-    public AiController(ChatClient.Builder builder) {
-        this.chatClient = builder.
-                defaultAdvisors(new SimpleLoggerAdvisor())
+    public AiController(ChatClient.Builder builder, ToolCallbackProvider toolCallbackProvider) {
+        this.chatClient = builder
+                .defaultToolCallbacks(toolCallbackProvider)
+                .defaultAdvisors(new SimpleLoggerAdvisor())
                 .build();
     }
 
     private ChatClient chatClient;
 
+    @PostMapping("/chat")
     public ResponseEntity<String> getResponse(@RequestParam("query") String query){
 
         String response = chatClient.prompt(query).call().content();
